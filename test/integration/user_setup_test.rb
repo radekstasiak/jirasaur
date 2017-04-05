d70eca86daee13f1d9591dbc02f4622db42d090d
Radek <radek.stasiak@gmail.com>
2017-04-05 08:52:41 +0100

    Update id columns to string type
    - update team id and user id to string type
    - update tests

--- test/integration/user_setup_test.rb
+++ test/integration/user_setup_test.rb
@@ -6,22 +6,24 @@ class UsersSetupTest < ActionDispatch::IntegrationTest
 	def setup
 		ENV['SLACK_TOKEN']="aaa"
 		@user = users(:radek)
+		@user2 = User.new(team_id:"X3", user_id:"Y3",user_name:"Kedar",team_domain:"radev")
 	end
 
 	test "request hould be authenticated with correct token" do
-		post "/api/v1/report", token:"",team_id:3, user_id:3,user_name:"Kedar",team_domain:"radev"
+		post "/api/v1/report", token:"",team_id:@user2.team_id, user_id:@user2.user_id,user_name:@user2.user_name,team_domain:@user2.team_domain
 		assert_equal response.body.to_s, "{\"status\":\"unauthorized\"}"
 	end
 
 	test "successfuly create new user" do 
 		assert_equal User.all.size, 1
-		post "/api/v1/report", token:"aaa",team_id:3, user_id:3,user_name:"Kedar",team_domain:"radev"
+		post "/api/v1/report", token:"aaa",team_id:@user2.team_id, user_id:@user2.user_id,user_name:@user2.user_name,team_domain:@user2.team_domain
 		assert_equal User.all.size, 2
 	end
 
 	test "successfuly retrieve existing user" do
 		assert_equal User.all.size, 1
-		post "/api/v1/report", token:"aaa",team_id:@user.team_id, user_id:@user.user_id,user_name:@user.user_name,team_domain:@user.team_domain
+		user =  User.all.at(0)
+		post "/api/v1/report", token:"aaa",team_id:@user.team_id, user_id:@user.user_id,user_name:@user.user_name,team_domain:@user.team_domain		
 		assert_equal User.all.size, 1
 	end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-  	@user = User.new(team_id:2, user_id:2,user_name:"Kedar",team_domain:"radev")
+  	@user = User.new(team_id:"X2", user_id:"Y2",user_name:"Kedar",team_domain:"radev")
   end
 
   test "should be valid" do
@@ -18,22 +18,38 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "user id should be present" do
-  	@user.user_id=nil
+  	@user.user_id=""
   	assert_not @user.valid?
   end
 
   test "user id should be unique" do
-  	@user.user_id=1
+  	@user.user_id="Y1"
   	assert_not @user.valid?
   end
 
+  test "user id should be not nil" do
+    @user.user_id=nil
+    assert_not @user.valid?
+  end
+
   test "team id should be present" do
-  	@user.team_id=nil
+  	@user.team_id=""
   	assert_not @user.valid?
+  end
+
+  test "team id should be unique" do
+    @user.team_id="X1"
+    assert_not @user.valid?
+  end
+
+  test "team id should be not nil" do
+    @user.team_id=nil
+    assert_not @user.valid?
   end
 
   test "team domain should be present" do
   	@user.team_domain=""
   	assert_not @user.valid?
   end
+
 end
